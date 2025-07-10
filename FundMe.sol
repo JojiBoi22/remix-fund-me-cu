631862e6c31f695492ee23567632b7d5ee1a0069
JojiBoi22 <dillonpedro@yahoo.com>
2025-07-10 14:01:49 +0200

    Added fund, withdraw functions with transfer, send and call options

--- FundMe.sol
+++ FundMe.sol
@@ -14,13 +14,14 @@ contract FundMe {
     address[] public funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
     constructor() {
-        i_owner = msg.sender;
+        owner = msg.sender;
     }
 
+    
     function fund() public payable {
         require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
@@ -33,28 +34,28 @@ contract FundMe {
         return priceFeed.version();
     }
 
-    modifier onlyOwner() {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
-        _;
-    }
-
     function withdraw() public onlyOwner {
+        require(msg.sender == owner, "Must be owner");
         for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
         // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
+        payable(msg.sender).transfer(address(this).balance);
 
         // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
 
         // call
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
     }
     // Explainer from: https://solidity-by-example.org/fallback/
     // Ether is sent to contract
